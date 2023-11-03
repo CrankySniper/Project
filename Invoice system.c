c7e2f5c3e62ca0407374f1c44e556a90c0e778df
Anand Roy <86306690+CrankySniper@users.noreply.github.com>
2023-11-03 10:29:15 +0530

    Add files via upload

---
+++ Invoice system.c
@@ -0,0 +1,346 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
+#include <conio.h>
+#include <windows.h>
+
+struct items
+{
+    char item[20];
+    float price;
+    int qty;
+};
+
+struct orders
+{
+    char customer[50];
+    char date[50];
+    int numOfItems;
+    struct items itm[50];
+};
+
+int pass(void);
+int tm()
+{
+    time_t mytime;
+    mytime = time(NULL);
+    printf(ctime(&mytime));
+    return 0;
+}
+int delay(unsigned int mseconds)
+{
+    clock_t goal = mseconds + clock();
+    while (goal > clock())
+        ;
+}
+// functions to generate biils
+void generateBillHeader(char name[50], char date[30])
+{
+    printf("\n\n");
+    printf("\t\t\t\t\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\n");
+    printf("\t\t\t\t\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\n");
+    printf("\t\t\t\t\t============ADV. RESTAURANT============");
+    printf("\n\t\t\t\t\t   -----------------");
+    printf("\nDate:%s", date);
+    printf("\nInvoice To: %s", name);
+    printf("\n");
+    printf("---------------------------------------\n");
+    printf("Items\t\t");
+    printf("Qty\t\t");
+    printf("Total\t\t");
+    printf("\n---------------------------------------");
+    printf("\n\n");
+}
+void generateBillBody(char item[30], int qty, float price)
+{
+    printf("%s\t\t", item);
+    printf("%d\t\t", qty);
+    printf("%.2f\t\t", qty * price);
+    printf("\n");
+}
+void generateBillFooter(float total)
+{
+    printf("\n");
+    float dis = 0.1 * total;
+    float netTotal = total - dis;
+    float cgst = 0.09 * netTotal, grandTotal = netTotal + 2 * cgst; // netTotal + cgst + sgst
+    printf("---------------------------------------\n");
+    printf("Sub Total\t\t\t%.2f", total);
+    printf("\nDiscount @10%s\t\t\t%.2f", "%", dis);
+    printf("\n\t\t\t\t-------");
+    printf("\nNet Total\t\t\t%.2f", netTotal);
+    printf("\nCGST @9%s\t\t\t%.2f", "%", cgst);
+    printf("\nSGST @9%s\t\t\t%.2f", "%", cgst);
+    printf("\n---------------------------------------");
+    printf("\nGrand Total\t\t\t%.2f", grandTotal);
+    printf("\n---------------------------------------\n");
+}
+int main()
+{
+    int opt, n;
+    struct orders ord;
+    struct orders order;
+    char saveBill = 'y', contFlag = 'y';
+    char name[50];
+    FILE *fp;
+    pass();
+    while (contFlag == 'y')
+    {
+        system("cls");
+        float total = 0;
+        int invoiceFound = 0;
+        system("CLS");
+        system("color 07");
+        printf("\n\n\t\t\t============ADV. RESTAURANT============");
+        printf("\n\n\t\t\t\t\xB2\xB2\xB2\xB2\xB2\xB2\xB2 MAIN MENU \xB2\xB2\xB2\xB2\xB2\xB2\xB2");
+        printf("\n\n");
+        printf("\t\t\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\n");
+        printf("\t\t\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\n");
+        printf("\t\t\xDB\xDB\xDB\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xDB\xDB\xDB\n");
+        printf("\t\t\xDB\xDB\xDB\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2 [1] Generate Invoice  \xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xDB\xDB\xDB\n");
+        printf("\t\t\xDB\xDB\xDB\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xDB\xDB\xDB\n");
+        printf("\t\t\xDB\xDB\xDB\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2 [2] Show all Invoices \xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xDB\xDB\xDB\n");
+        printf("\t\t\xDB\xDB\xDB\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xDB\xDB\xDB\n");
+        printf("\t\t\xDB\xDB\xDB\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2 [3] Search Invoice    \xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xDB\xDB\xDB\n");
+        printf("\t\t\xDB\xDB\xDB\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xDB\xDB\xDB\n");
+        printf("\t\t\xDB\xDB\xDB\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2 [4] Exit              \xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xDB\xDB\xDB\n");
+        printf("\t\t\xDB\xDB\xDB\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xDB\xDB\xDB\n");
+        printf("\t\t\xDB\xDB\xDB\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xDB\xDB\xDB\n");
+        printf("\t\t\xDB\xDB\xDB\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xDB\xDB\xDB\n");
+        printf("\t\t\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\n");
+        printf("\t\t\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\n");
+        printf("\n \t\t Today is: ");
+        tm();
+        printf("\n\t\t Please Enter Your Choice: ");
+        scanf("%d", &opt);
+        fgetc(stdin);
+        switch (opt)
+        {
+        case 1:
+            system("cls");
+            printf("\nPlease enter the name of the customer:\t");
+            fgets(ord.customer, 50, stdin);
+            ord.customer[strlen(ord.customer) - 1] = 0;
+            strcpy(ord.date, __DATE__);
+            printf("\nPlease enter the number of items:\t");
+            scanf("%d", &n);
+            ord.numOfItems = n;
+            for (int i = 0; i < n; i++)
+            {
+                fgetc(stdin);
+                printf("\n\n");
+                printf("Please enter the item %d:\t", i + 1);
+                fgets(ord.itm[i].item, 20, stdin);
+                ord.itm[i].item[strlen(ord.itm[i].item) - 1] = 0;
+                printf("Please enter the quantity:\t");
+                scanf("%d", &ord.itm[i].qty);
+                printf("Please enter the unit price:\t");
+                scanf("%f", &ord.itm[i].price);
+                total += ord.itm[i].qty * ord.itm[i].price;
+            }
+            system("cls");
+            generateBillHeader(ord.customer, ord.date);
+            for (int i = 0; i < ord.numOfItems; i++)
+            {
+                generateBillBody(ord.itm[i].item, ord.itm[i].qty, ord.itm[i].price);
+            }
+            generateBillFooter(total);
+
+            printf("\nDo you want to save the invoice [y/n]:\t");
+            scanf("%s", &saveBill);
+
+            if (saveBill == 'y')
+            {
+                fp = fopen("RestaurantBill.dat", "a+");
+                fwrite(&ord, sizeof(struct orders), 1, fp);
+                if (fwrite != 0)
+                    printf("\nSuccessfully saved");
+                else
+                    printf("\nError saving");
+                fclose(fp);
+            }
+            break;
+
+        case 2:
+            system("cls");
+            fp = fopen("RestaurantBill.dat", "r");
+            printf("\n  **Your Previous Invoices**\n");
+            while (fread(&order, sizeof(struct orders), 1, fp))
+            {
+                float tot = 0;
+                generateBillHeader(order.customer, order.date);
+                for (int i = 0; i < order.numOfItems; i++)
+                {
+                    generateBillBody(order.itm[i].item, order.itm[i].qty, order.itm[i].price);
+                    tot += order.itm[i].qty * order.itm[i].price;
+                }
+                generateBillFooter(tot);
+            }
+            fclose(fp);
+            break;
+
+        case 3:
+            printf("Enter the name of the customer:\t");
+            fgets(name, 50, stdin);
+            name[strlen(name) - 1] = 0;
+            system("cls");
+            fp = fopen("RestaurantBill.dat", "r");
+            printf("\t**Invoice of %s**", name);
+            while (fread(&order, sizeof(struct orders), 1, fp))
+            {
+                float tot = 0;
+                if (!strcmp(order.customer, name))
+                {
+                    generateBillHeader(order.customer, order.date);
+                    for (int i = 0; i < order.numOfItems; i++)
+                    {
+                        generateBillBody(order.itm[i].item, order.itm[i].qty, order.itm[i].price);
+                        tot += order.itm[i].qty * order.itm[i].price;
+                    }
+                    generateBillFooter(tot);
+                    invoiceFound = 1;
+                }
+            }
+            if (!invoiceFound)
+            {
+                printf("Sorry the invoice for %s doesnot exists", name);
+            }
+            fclose(fp);
+            break;
+
+        case 4:
+            printf("\n\t\t Code ends\n\n");
+            exit(0);
+            break;
+
+        default:
+            printf("Sorry invalid option");
+            break;
+        }
+        printf("\n<Enter any key to continue>");
+        getch();
+
+        printf("\nDo you want to perform another operation? [y/n]");
+        scanf("%s", &contFlag);
+    }
+    printf("\n\t\t Code ends\n\n");
+    printf("\n\n");
+}
+int pass(void)
+{
+    int a = 0, i = 0;
+    char uname[10], c = ' ';
+    char pword[10], code[10];
+    char user[10] = "user";
+    char pass[10] = "pass";
+    system("cls");
+    system("color e0");
+    int i1, j, l, l1;
+    char name[50] = "\n\n\n\n\t\t\t  WELCOME TO OUR PROJECT\n\n";
+    printf("\t\n");
+    for (i1 = 0; i1 < 50; i1++)
+    {
+        printf("%c", name[i1]);
+        delay(20);
+    }
+    printf("\n\t   ");
+    for (j = 0; j < 59; j++)
+    {
+        printf("%c", 2);
+        delay(30);
+    }
+    printf("\n\n\n\n");
+    system("cls");
+    system("color 0f");
+
+    printf("\n\n");
+    system("CLS");
+    system("color 1f");
+    printf("\n\n\n");
+    char ame[25] = "\tGROUP MEMBERS\n\n";
+    printf("\t\n");
+    for (l1 = 0; l1 < 25; l1++)
+    {
+        printf("%c", ame[l1]);
+        delay(150);
+    }
+    printf(" \n\t 23EE01004:  - Anand Roy\n\n");
+    printf(" \t 23EC01005:  - Aum Vyas\n\n");
+    printf(" \t 23ME01003:  - Aditya\n\n");
+    printf(" \t 23ME01044:  - Naman\n\n");
+    printf("\n");
+    system("pause");
+    system("CLS");
+    system("color 5e");
+    printf("\n\n\n\n\n\n");
+    printf("\t\t\tOur project on:\n\n");
+    printf("\n");
+    char nam[] = "\n\n\t\t\tInvoice Management System Project \n\n";
+    printf("\t\n");
+    for (l = 0; l < 40; l++)
+    {
+        printf("%c", nam[l]);
+        delay(40);
+    }
+    printf("\n\n\n");
+    delay(600);
+    do
+    {
+        system("cls");
+        printf("\n\n\n \t \t \t   ***  LOGIN FORM  ***  ");
+        printf("\n\n\n  \t           ENTER USERNAME:-");
+        scanf("%s", &uname);
+        printf("\n  \t          ENTER PASSWORD:-");
+        while (i < 10)
+        {
+            pword[i] = getch();
+            c = pword[i];
+            if (c == 13)
+                break;
+            else
+                printf("*");
+            i++;
+        }
+        pword[i] = '\0';
+        i = 0;
+        if (strcmp(uname, user) == 0 && strcmp(pword, pass) == 0)
+        {
+            printf("  \n\n\n       WELCOME !!!! LOGIN IS SUCCESSFUL\n");
+            system("cls");
+            system("color 1a");
+            printf("\n\n\n\n\n\n");
+            printf("                                                           \n");
+            printf("                                                           \n");
+            printf("                      \t  Please Wait...\n\n\n\n\n");
+            printf("                     ______________ _____________               \n");
+            printf("                    /                             \\          \n");
+            printf("                   |     Loading.............      |           \n");
+            printf("\t\t   |\t");
+            for (j = 0; j < 24; j++)
+            {
+                printf("%c", 219);
+                delay(50);
+            }
+            printf("   |\n                    \\______________ ______________/           \n");
+            printf("                                                           \n");
+            printf("\n\n\n");
+            delay(1000);
+            break;
+        }
+        else
+        {
+            printf("\n        SORRY !!!!  LOGIN IS UNSUCESSFUL\n");
+            system("PAUSE");
+            a++;
+            getch();
+        }
+    } while (a <= 2);
+    if (a > 2)
+    {
+        printf("\nSorry you have entered the wrong user name and password for four times!!!");
+        getch();
+    }
+    system("cls");
+}
